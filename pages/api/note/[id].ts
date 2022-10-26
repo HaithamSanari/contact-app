8555961844b48085280546544ec61f1b8162f7c4
HaithamSanari <haithamsanari@gmail.com>
2022-10-26 18:36:40 +0300

    #

--- pages/api/note/[id].ts
+++ pages/api/note/[id].ts
@@ -1,36 +1,3 @@
-// import { NextApiRequest, NextApiResponse } from "next";
-// import { prisma } from "../../../lib/prisma";
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse){
-// 	const noteId = req.query.id
-//   const {title, content} = req.body;
-
-// 	if(req.method === 'DELETE') {
-// 		const note = await prisma.note.delete({
-// 			where: {id: String(noteId)}
-// 		})
-// 		res.json(note)
-// 	} else {
-// 		console.log("Note could not be created");
-// 	}
-
-//   if(req.method === 'PUT') {
-//     const note = await prisma.note.update({
-//       where:{
-//           id: String(noteId)
-//       },
-//       data:{
-//           title: title,
-//           content: content
-//       }
-//   });
-//   res.status(200).json(note);
-// 	} else {
-// 		console.log("Note could not be created");
-// 	}
-
-// }
-
 import { NextApiRequest, NextApiResponse } from 'next';
 import prisma from '../../../lib/prisma';
 
