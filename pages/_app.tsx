76d2979e1a7f006a69a0998c2175017cd3d0b42b
HaithamSanari <haithamsanari@gmail.com>
2022-10-26 18:07:44 +0300

    fix: session

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
 import '../styles/globals.css';
 import type { AppProps } from 'next/app';
-import Header from '../components/Header';
+import type { Session } from 'next-auth';
 import { SessionProvider } from 'next-auth/react';
+import Header from '../components/Header';
 import AuthWrapper from '../components/AuthWrapper';
 import { ChakraProvider } from '@chakra-ui/react';
-
-function MyApp({ Component, pageProps }: AppProps) {
+function MyApp({ Component, pageProps }: AppProps<{ session: Session }>) {
   return (
     <SessionProvider session={pageProps.session}>
       <AuthWrapper>
